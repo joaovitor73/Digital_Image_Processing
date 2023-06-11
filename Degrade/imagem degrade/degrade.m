e8c968f2f8c5ea08314dbaa1aa4bdeb8e2250c11
João Vitor <83173020+joaovitor73@users.noreply.github.com>
2023-06-11 16:52:41 -0300

    Add files via upload

---
+++ Degrade/imagem degrade/degrade.m
@@ -0,0 +1,19 @@
+close all;
+clear all;
+
+%1. Criar uma imagem vazia;
+%2. Percorrer a imagem gerando um degrad? de 256 tons de cinza;
+%3. Abrir uma nova janela;
+%4. Mostrar a imagem em degrad?;
+
+im = uint8(zeros(50, 256));
+
+for i = 1 : size(im,1)
+  for j = 1 : size(im,2)
+      im(i,j) = j;
+  endfor
+endfor
+
+figure(1);
+imshow(im);
+
---
+++ Degrade/lena degrade/lenaDegrade.m
@@ -0,0 +1,23 @@
+close all;
+clear all;
+
+im = imread('lena.jpg');
+imCinza = (im(:,:,1)*0.3+im(:,:,2)*0.59+im(:,:,3)*0.11);
+
+for i = 1 : size(im,1)
+  cont = 255;
+  for j = 1 : size(im,2)
+    if(j < size(im,2)/2)
+      imCinza(i,j) = imCinza(i,j)-cont;
+      cont--;
+    else
+      imCinza(i,j) = imCinza(i,j)+cont;
+      cont++;
+    endif
+  endfor
+endfor
+
+
+figure(1);
+imshow(imCinza);
+
