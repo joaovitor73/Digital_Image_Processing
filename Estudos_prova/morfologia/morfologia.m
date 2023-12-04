7d8687cdc4f4d918edfe05f26cd79ca8deab190b
João Vitor <83173020+joaovitor73@users.noreply.github.com>
2023-12-04 14:38:37 -0300

    Add files via upload

--- Estudos_prova/morfologia/morfologia.m
+++ Estudos_prova/morfologia/morfologia.m
@@ -12,8 +12,9 @@ figure, imshow(im);
 height = size(im, 1);
 width = size(im, 2);
 
-erodir = true;
+erodir = false;
 kernel_rect = true;
+
 
 if kernel_rect
     altura = 5;
@@ -25,22 +26,7 @@ if kernel_rect
     heightM = size(imMaior,1);
     widthM = size(imMaior, 2);
     imMaior(comecoX+1: heightM-comecoX, comecoY+1:widthM-comecoY) = im;
-else
-    raio = 3;
-    tam = raio*2+1;
-    EE = logical(ones(tam, tam));
-    comecoX = floor(raio/2);
-    comecoY = floor(raio/2);
-    distanciaVez = 0;
-    for i = 1:ta
-        for j = 1:tam
-            distanciaVez = sqrt((i - (raio+1)).^2 + (j - (raio+1)).^2);
-            EE(i, j) = distanciaVez <= raio;
-        endfor
-    endfor
-endif
-
-if erodir
+    if erodir
     for i =  comecoX+1:heightM-comecoX
         for j =  comecoY+1:widthM-comecoY
             if imMaior(i, j)
@@ -51,18 +37,90 @@ if erodir
             endif
         endfor
     endfor
+  else
+      for i = comecoX+1:height+comecoX
+          for j = comecoY+1:width+comecoY
+              if imMaior(i, j) ==  0
+                  viz = imMaior(i-comecoX:i+comecoX, j-comecoY:j+comecoY);
+                  if sum(sum(viz)) != 0
+                      im(i-comecoX, j-comecoX) = 1;
+                  endif
+              endif
+          endfor
+      endfor
+  end
 else
-    for i = comecoX+1:height+comecoX
-        for j = comecoY+1:width+comecoY
-            if imMaior(i, j) ==  0
-                viz = imMaior(i-comecoX:i+comecoX, j-comecoY:j+comecoY);
-                if sum(sum(viz)) != 0
-                    im(i-comecoX, j-comecoX) = 1;
-                endif
-            endif
+    raio = 3;
+    tam = raio*2+1;
+    EE = logical(ones(tam, tam));
+    comecoX = raio;
+    comecoY = raio;
+    distanciaVez = 0;
+    cont1 = 0;
+    cont2 = 0;
+
+    imMaior = zeros(height+comecoX*2+1, width+comecoY*2+1);
+    heightM = size(imMaior,1);
+    widthM = size(imMaior, 2);
+    imMaior(comecoX+1: heightM-comecoX-1, comecoY+1:widthM-comecoY-1) = im;
+
+    for i = 1:tam
+        for j = 1:tam
+            distanciaVez = sqrt((i - (raio+1)).^2 + (j - (raio+1)).^2);
+            EE(i, j) = distanciaVez <= raio;
         endfor
     endfor
-end
+
+    if erodir
+       for i =  comecoX+1:heightM-comecoX
+        for j =  comecoY+1:widthM-comecoY
+          viz = imMaior(i- comecoX:i+ comecoX, j-comecoY:j+comecoY);
+          cont1 = 0;
+          cont2 = 0;
+          for l=1:size(EE,1)
+            for c=1:size(EE,2)
+               distanciaVez = sqrt((l - (raio+1)).^2 + (c - (raio+1)).^2);
+                if(distanciaVez <= raio)
+                    if(EE(l,c))
+                      cont1++;
+                    endif
+                    if(viz(l,c))
+                      cont2++;
+                    endif
+                endif
+            endfor
+          endfor
+          if(cont1 != cont2)
+              im(i-comecoX, j-comecoX) = 0;
+          endif
+         endfor
+       endfor
+    else
+        for i =  comecoX+1:heightM-comecoX
+        for j =  comecoY+1:widthM-comecoY
+          viz = imMaior(i- comecoX:i+ comecoX, j-comecoY:j+comecoY);
+          cont1 = 0;
+          cont2 = 0;
+          for l=1:size(EE,1)
+            for c=1:size(EE,2)
+               distanciaVez = sqrt((l - (raio+1)).^2 + (c - (raio+1)).^2);
+                if(distanciaVez <= raio)
+                    if(EE(l,c))
+                      cont1++;
+                    endif
+                    if(viz(l,c))
+                      cont2++;
+                    endif
+                endif
+            endfor
+          endfor
+          if(cont1 != cont2 && cont2!=0)
+              im(i-comecoX, j-comecoX) = 1;
+          endif
+         endfor
+       endfor
+    end
+endif
 
 
 figure, imshow(im);
